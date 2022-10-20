bceef8ee886aa0d7f857088131b291cf6e905f0b
mkl061 <mariusdk@stud.ntnu.no>
2022-10-20 21:55:18 +0200

    nothing in particular

--- Unique_19.10.R
+++ Unique_19.10.R
@@ -36,9 +36,9 @@ cat(apt, sep = ", ")
 
 ## GO term related to "leukocyte":
 leu <- c(
-  str_to_vec(
-    "RB1|IL10|TNFA|CASP8|MYC|TRAF6|IRF1|RIPK1" # regulation of leukocyte differentiation
-  ),
+  # str_to_vec(
+  #   "RB1|IL10|TNFA|CASP8|MYC|TRAF6|IRF1|RIPK1" # regulation of leukocyte differentiation
+  # ),
   str_to_vec(
     "IL10|TNFA|CASP8|TRAF6|IRF1|STAT3|MYD88|ICAM1" # leukocyte activation
   ),
@@ -47,25 +47,27 @@ leu <- c(
   ),
   str_to_vec(
     "IL10|CSF1R|TNFA|CASP8|TRAF6|IRF1|STAT3" # leukocyte differentiation
-  ),
-  str_to_vec(
-    "RB1|TNFA|CASP8|MYC|TRAF6|RIPK1" # regulation of myeloid leukocyte differentiation
-  ),
-  str_to_vec(
-    "RB1|IL10|TNFA|CASP8|TRAF6|RIPK1" # positive regulation of leukocyte differentiation
-  ),
-  str_to_vec(
-    "CSF1R|TNFA|DUSP1|MYD88|ICAM1|MAPK3" # regulation of leukocyte migration
-  ),
-  str_to_vec(
-    "RB1|TNFA|CASP8|TRAF6|RIPK1" # positive regulation of myeloid leukocyte differentiation
-  )
+  )#,
+  # str_to_vec(
+  #   "RB1|TNFA|CASP8|MYC|TRAF6|RIPK1" # regulation of myeloid leukocyte differentiation
+  # ),
+  # str_to_vec(
+  #   "RB1|IL10|TNFA|CASP8|TRAF6|RIPK1" # positive regulation of leukocyte differentiation
+  # ),
+  # str_to_vec(
+  #   "CSF1R|TNFA|DUSP1|MYD88|ICAM1|MAPK3" # regulation of leukocyte migration
+  # ),
+  # str_to_vec(
+  #   "RB1|TNFA|CASP8|TRAF6|RIPK1" # positive regulation of myeloid leukocyte differentiation
+  # )
 ) %>% unique()
 
-
+cat(leu, sep=", ")
 
 
 ## GO term JNK: 
 JNK <- str_to_vec(
   "TNFA|TRAF6|RIPK1|MAP2K7|MYD88" # positive regulation of JNK cascade
 )
+
+cat(JNK, sep=", ")
