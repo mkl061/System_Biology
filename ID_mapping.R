b9066771c002a9f23aed56eedb496ba692744011
mkl061 <mariusdk@stud.ntnu.no>
2022-10-20 19:15:12 +0200

    Major updates (Only Uniprot mapping working)

--- ID_mapping.R
+++ ID_mapping.R
@@ -1,5 +1,11 @@
+library(tidyverse)
+
+
+#BiocManager::install("org.Hs.eg.db")
 library(org.Hs.eg.db)
 
+
+setwd("C:/Users/Legion/Downloads")
 # API URL for ALL human proteins in the Swiss-Prot database:
 # https://rest.uniprot.org/uniprotkb/stream?fields=accession%2Cid%2Cprotein_name%2Cgene_names&format=tsv&query=%28reviewed%3Atrue%29%20AND%20%28model_organism%3A9606%29
 
@@ -8,7 +14,7 @@ swiss_prot <- read.delim("Swiss.tsv")
 ## Your spreadsheet:
 #dat <- read.csv("original_set.csv")
 dat <- read.csv("nodes.csv")
-dat$Common_name <- dat$Common_name %>% str_replace("\xa0", "") # Fixing a reocurring problem
+dat$Common_name <- dat$Common_name %>% str_replace("\xa0", "") # Fixing a recurring problem
 
 ## Trim all whitespaces in all cells with strings:
 for (col_ind in 1:ncol(dat)) {
@@ -16,6 +22,69 @@ for (col_ind in 1:ncol(dat)) {
     dat[,col_ind] <- sapply(dat[,col_ind], str_trim)
   }
 }
+
+
+ID_mapping <- function(from, to, df, keys_column, from_column) {
+  ## Uniprot IDs given when mapping by "gene symbol":
+  uni_first <- mapIds(org.Hs.eg.db,
+                      #keys = df$id,
+                      keys = unlist(df[keys_column]),
+                      column = to,
+                      keytype = from,
+                      multiVals = "list") # A gene symbol may refer to multiple IDs (return a vector of these)
+  
+  
+  # We usually start by finding Uniprot IDs!
+  if (to == "UNIPROT") {
+    swiss_prot <- read.delim("https://rest.uniprot.org/uniprotkb/stream?fields=accession%2Cid%2Cprotein_name%2Cgene_names&format=tsv&query=%28reviewed%3Atrue%29%20AND%20%28model_organism%3A9606%29")
+    
+    
+    for (lst_ind in 1:length(uni_first)) { # List index val
+      new_vec <- vector() # Empty vector
+      for (vec_elm in uni_first[[lst_ind]]) { # Elements of the list-element
+        if (vec_elm %in% swiss_prot$Entry == T) { # Checks if element (i.e. Uniprot ID) is in Swiss-Prot (i.e. is it reviewed by expert)
+          new_vec <- append(new_vec, vec_elm) # Adds the element to the vector
+        }
+      }
+      
+      if (length(new_vec) == 0) { # If the vector is empty, set NA
+        new_vec <- NA
+      }
+      
+      uni_first[[lst_ind]] <- new_vec
+      
+    }
+  }
+  
+  
+  ## Unlist the list:
+  uni_first <- unlist(uni_first)
+  
+  
+  tabl_check <- df %>% 
+    dplyr::select("id"=!!sym(keys_column), "Our_ID"=!!sym(from_column)) %>% 
+    add_column("Mapping_ID" = uni_first) %>% 
+    mutate(final = ifelse(Our_ID == "", # Is Our_ID an empty string?
+                          Mapping_ID, # Yes; Set as the ID gotten from ID mapping
+                          ifelse(is.na(Our_ID),  # No; Initiate another ifelse(). Is Our_ID a NA?
+                                 Mapping_ID,  # Yes; Set as the ID gotten from ID mapping
+                                 Our_ID # No; keep Our_ID as it was
+                                 ) 
+                          )
+          )
+  
+  df$UniprotKB <- tabl_check$final
+  
+  return(df)
+}
+
+
+View(ID_mapping("SYMBOL", "UNIPROT", dat, keys_column = "id", from_column = "UniprotKB"))
+ny <- ID_mapping("SYMBOL", "UNIPROT", dat, keys_column = "id", from_column = "UniprotKB")
+liste <- ID_mapping("UNIPROT", "ENSEMBL", ny, keys_column = "UniprotKB", from_column = "Ensembl")
+View(ID_mapping("UNIPROT", "SYMBOL", dat, keys_column = "UniprotKB", from_column = "Ensembl"))
+
+
 
 ## Uniprot IDs given when mapping by "gene symbol":
 uni_first <- mapIds(org.Hs.eg.db,
@@ -44,6 +113,14 @@ for (lst_ind in 1:length(uni_sec)) {
   
   uni_sec[[lst_ind]] <- new_vec
   
+  df <- df %>% 
+    mutate(UniprotKB = ifelse(
+      tabl_check$Our_ID == "" && (tabl_check$Mapping_ID != "" || is.na(tabl_check$Mapping_ID)),
+      tabl_check$final <- tabl_check$Mapping_ID,
+      tabl_check$final <- tabl_check$Our_ID
+    ))
+  
+  return(df)
 }
 
 ## Unlist the list:
@@ -83,4 +160,10 @@ for (i in 1:nrow(tabl_check)) {
 merg_table <- cbind.data.frame(tabl_check$id, tabl_check$final)
 colnames(merg_table) <- c("id", "UniprotKB")
 
-dat2 <- left_join(dat, merg_table)
+new_dat <- dat
+new_dat$UniprotKB <- tabl_check$final
+
+hei <- hei %>% 
+  mutate(UniprotKB.x = ifelse((UniprotKB.y=="" || is.na(UniprotKB)),))
+#hei <- merge(dat, merg_table, by="id", all.x = T)
+#dat2 <- left_join(dat, merg_table)
